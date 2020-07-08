8684a04eb2dcac4a4dd5fef3f6196ca96c73110b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-08 22:11:06 +0700

    membuat model untuk pertanyaan

---
+++ app/Pertanyaan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pertanyaan extends Model
+{
+    /*
+    * menggunakan tabel pertanyaan
+    */
+    protected $table = "pertanyaan";
+}
