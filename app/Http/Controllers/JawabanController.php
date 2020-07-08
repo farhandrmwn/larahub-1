3850e0c658fc00d23e8dd4610205d67419248fbc
DzulkhiPratama <dzulkhi.yudha@gmail.com>
2020-07-08 23:39:04 +0700

    Men set dasar controller

--- app/Http/Controllers/JawabanController.php
+++ app/Http/Controllers/JawabanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Jawaban;
 
 class JawabanController extends Controller
 {
@@ -13,7 +14,8 @@ class JawabanController extends Controller
      */
     public function index()
     {
-        //
+        $jawaban = Jawaban::all();
+        // return view('', compact('jawaban'));
     }
 
     /**
