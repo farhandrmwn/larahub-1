03c6122185c072582e76963ca406705d633d7735
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-08 21:41:48 +0700

    membuat migration untuk pertanyaan

---
+++ database/migrations/2020_07_08_143059_create_pertanyaan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePertanyaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertanyaan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('judul', 255);
+            $table->string('isi', 512);
+            $table->string('tag', 255);
+            $table->dateTime("updated_at");
+            $table->dateTime("created_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertanyaan');
+    }
+}
