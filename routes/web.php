049c923bdf6204a7f79a8a301b37f21ea543026f
DzulkhiPratama <dzulkhi.yudha@gmail.com>
2020-07-09 10:52:31 +0700

    Pemasangan perlengkapan auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.master');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
