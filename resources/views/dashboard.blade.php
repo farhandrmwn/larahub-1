8515e00926b9703f938e18d9a7c02ac1f3f30a91
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-09 17:51:46 +0700

    mengubah button untuk pergi ke halaman detail

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,61 +2,58 @@
 
 @section("content")
 <div class="container">
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Dashboard</h1>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <div class="card">
+        <button type="button" class="btn btn-primary"><a href="/pertanyaan/create" style="color:white;">Buat Pertanyann</a></button>
+    </div>
+
+    @foreach($pertanyaan as $ask)
+
+    <div class="card">
+        <div class="card-header">
+            <a class="card-title" href="/pertanyaan/{{$ask->id}}">
+                <h4>{{$ask->judul}}</h4>
+            </a>
+
+            <div class="card-tools">
+                <form action=<?= "/pertanyaan/" . $ask->id . "/vote/up" ?> method="POST" style="display:inline">
+                    @csrf
+                    <input type="hidden" name="_method" value="PUT">
+                    <button type="submit" class="btn btn-success">UP</button>
+                </form>
+                <form action=<?= "/pertanyaan/" . $ask->id . "/vote/down" ?> method="POST" style="display:inline">
+                    @csrf
+                    <input type="hidden" name="_method" value="PUT">
+                    <button type="submit" class="btn btn-danger">DOWN</button>
+                </form>
+            </div>
         </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-          </ol>
+        <div class="card-body">
+            <p>
+                {{$ask->isi}}
+            </p>
         </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-  <div class="card">
-    <button type="button" class="btn btn-primary"><a href="/pertanyaan/create" style="color:white;">Buat Pertanyann</a></button>
-  </div>
-
-  @foreach($pertanyaan as $ask)
-
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">{{$ask->judul}}</h3>
-
-      <div class="card-tools">
-        <form action=<?= "/pertanyaan/" . $ask->id . "/vote/up" ?> method="POST" style="display:inline">
-          @csrf
-          <input type="hidden" name="_method" value="PUT">
-          <button type="submit" class="btn btn-success">UP</button>
-        </form>
-        <form action=<?= "/pertanyaan/" . $ask->id . "/vote/down" ?> method="POST" style="display:inline">
-          @csrf
-          <input type="hidden" name="_method" value="PUT">
-          <button type="submit" class="btn btn-danger">DOWN</button>
-        </form>
-
-        <button type="submit" class="btn btn-sm btn-default">
-          <a href="/pertanyaan/{{$ask->id}}">View</a>
-        </button>
-
-      </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            Footer
+        </div>
+        <!-- /.card-footer-->
     </div>
-    <div class="card-body">
-      <p>
-        {{$ask->isi}}
-      </p>
-    </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-      Footer
-    </div>
-    <!-- /.card-footer-->
-  </div>
 
-  @endforeach
+    @endforeach
 </div>
 
 @endsection
