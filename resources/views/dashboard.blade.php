237c8d3355349dc74fd35f8e82d98d39775db2b6
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-09 12:57:24 +0700

    update blade template untuk vote

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,41 +2,49 @@
 
 @section("content")
 <div class="container">
-	<section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Dashboard</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
     </section>
     <div class="card">
-    <button type="button" class="btn btn-primary"><a href="/pertanyaan/create" style="color:white;">Buat Pertanyann</a></button>
+        <button type="button" class="btn btn-primary"><a href="/pertanyaan/create" style="color:white;">Buat Pertanyann</a></button>
     </div>
-	<div class="card">
+    <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Judul Pertanyaan</h3>
+            <h3 class="card-title">Judul Pertanyaan</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-success">UP</button>
-            <button type="button" class="btn btn-danger">DOWN</button>
-          </div>
+            <div class="card-tools">
+                <form action="/pertanyaan/11/vote/up" method="POST">
+                    @csrf
+                    <input type="hidden" name="_method" value="PUT">
+                    <button type="submit" class="btn btn-success">UP</button>
+                </form>
+                <form action="/pertanyaan/11/vote/down" method="POST">
+                    @csrf
+                    <input type="hidden" name="_method" value="PUT">
+                    <button type="submit" class="btn btn-danger">DOWN</button>
+                </form>
+            </div>
         </div>
         <div class="card-body">
-          Isi pertanyaan
+            Isi pertanyaan
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
-          Footer
+            Footer
         </div>
         <!-- /.card-footer-->
-      </div>
+    </div>
 </div>
 
 @endsection
