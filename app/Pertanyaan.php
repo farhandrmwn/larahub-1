0230d91f1ce7c15cd0e96fdef880507eb02620cb
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-10 18:28:38 +0700

    membuat relasi terhadap komentar dan user

--- app/Pertanyaan.php
+++ app/Pertanyaan.php
@@ -16,4 +16,14 @@ class Pertanyaan extends Model
         return $this->hasMany('App\Jawaban');
         // return $this->hasMany('App\Jawaban', 'id');
     }
+
+    public function komentar()
+    {
+        return $this->hasMany("App\Komentar", "pertanyaan_id");
+    }
+
+    public function user()
+    {
+        return $this->belongsTo("App\User");
+    }
 }
