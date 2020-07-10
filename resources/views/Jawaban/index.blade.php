77a134dde462b081b8bab239d658a8251f0e1599
farhandrmwn <farhanbrobro@gmail.com>
2020-07-10 14:10:01 +0700

    modal jawaban

--- resources/views/Jawaban/index.blade.php
+++ resources/views/Jawaban/index.blade.php
@@ -38,7 +38,7 @@
         </div>
         <div class="card-body">
             <p>{{ $ask->isi }}</p>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#komenpertanyaan">
                 Beri Komentar
             </button>
         </div>
@@ -74,6 +74,9 @@
             </form>
         </div>
         <div class="card-footer">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#komenjawaban">
+              Beri Komentar
+            </button>
             @foreach($listKomentarJawaban as $komentarJawaban)
             @if($komentarJawaban->jawaban_id === $data->id)
             <span class="text-primary">{{ $komentarJawaban->user_name }}</span>
@@ -84,25 +87,48 @@
     </div>
     @endforeach
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <form class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Komentar anda</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+    <!-- Modal Komentar Pertanyaan -->
+    <div class="modal fade" id="komenpertanyaan" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Pertanyaan</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+                <div class="form-group">
+                    <label for="isi" class="col-form-label">Komentar:</label>
+                    <textarea class="form-control" name ="isi" id="message-text"></textarea>
                 </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="isi" class="col-form-label">Komentar:</label>
-                        <textarea class="form-control" name="isi" id="message-text"></textarea>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <!-- Modal Komentar Jawaban-->
+    <div class="modal fade" id="komenjawaban" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Jawaban</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+                <div class="form-group">
+                    <label for="isi" class="col-form-label">Komentar:</label>
+                    <textarea class="form-control" name ="isi" id="message-text"></textarea>
                 </div>
             </form>
         </div>
