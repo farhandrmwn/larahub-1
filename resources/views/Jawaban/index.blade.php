5d9ff13ebb865d02b6d3f90ea38ca885daa0a575
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-10 13:46:08 +0700

    Mengubah lokasi button beri komentar dan membuat modal menjadi form

--- resources/views/Jawaban/index.blade.php
+++ resources/views/Jawaban/index.blade.php
@@ -38,12 +38,12 @@
         </div>
         <div class="card-body">
             <p>{{ $ask->isi }}</p>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Beri Komentar
+            </button>
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-              Beri Komentar
-            </button>
             <p>{{ count($listKomentarPertanyaan) }} Komentar</p>
             @foreach($listKomentarPertanyaan as $komentarPertanyaan)
             <span class="text-primary">{{ $komentarPertanyaan->user->name }}</span>
@@ -86,29 +86,27 @@
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form>
-                <div class="form-group">
-                    <label for="isi" class="col-form-label">Komentar:</label>
-                    <textarea class="form-control" name ="isi" id="message-text"></textarea>
+        <div class="modal-dialog" role="document">
+            <form class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Komentar anda</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="isi" class="col-form-label">Komentar:</label>
+                        <textarea class="form-control" name="isi" id="message-text"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
             </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
         </div>
-      </div>
     </div>
-    </div>
+</div>
 
 @endsection
