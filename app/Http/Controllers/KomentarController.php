9ff9c317b70668fa9b323a68555763b2eca1b7d8
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-10 19:26:57 +0700

    menghapus kode yang berisi query

--- app/Http/Controllers/KomentarController.php
+++ app/Http/Controllers/KomentarController.php
@@ -28,35 +28,4 @@ class KomentarController extends Controller
 
         return $listKomentar;
     }
-
-    static function getPertanyaan($pertanyaan_id)
-    {
-        $listKomentar = Komentar::with(['user'])
-            ->where("komentar.pertanyaan_id", "=", $pertanyaan_id)
-            ->where("komentar.jawaban_id", "=", NULL)
-            ->get();
-        return $listKomentar;
-    }
-
-    static function getJawaban($pertanyaan_id)
-    {
-        $listKomentar = DB::select(
-            "SELECT
-                komentar.isi AS komentar_isi,
-                komentar.user_id AS user_id,
-                komentar.jawaban_id AS jawaban_id,
-                users.name AS user_name,
-                jawaban.pertanyaan_id AS pertanyaan_id
-            FROM komentar
-            INNER JOIN jawaban
-                ON komentar.jawaban_id = jawaban.id
-            INNER JOIN pertanyaan
-                ON komentar.jawaban_id = pertanyaan.id
-            INNER JOIN users
-                ON komentar.user_id = users.id
-            WHERE jawaban.pertanyaan_id = $pertanyaan_id"
-        );
-
-        return $listKomentar;
-    }
 }
